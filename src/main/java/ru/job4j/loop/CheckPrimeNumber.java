64a84f2496cfd676c48bea027e540f9e5a229254
Kate Stulova <stulova.ek@gmail.com>
2021-10-30 22:12:13 +0300

    correct all code

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,14 +2,15 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = false;
-        int i = 2;
-        while (number % i != 0) {
-            i++;
-            if (i == number) {
-                prime = true;
+        boolean prime = true;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                prime = false;
                 break;
             }
+        }
+        if (number == 1) {
+            prime = false;
         }
         return prime;
     }
